112108d71dbda7f84ad37f83299ba4c70b7e76f3
Xyves <xodrixx@protonmail.com>
2024-10-06 19:40:44 +0200

    Add logout route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,5 +6,18 @@ const authController = require("../controllers/auth_controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 appRouter.post("/login", authController.login);
-appRouter.post("/signup", authController.signup);
+appRouter.post(
+  "/signup",
+  // authController.createUserValidation(),
+  authController.validateMiddleware,
+  authController.signup
+);
+appRouter.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 module.exports = appRouter;
--- routes/blogsRouter.js
+++ routes/blogsRouter.js
@@ -13,7 +13,7 @@ const { verifyToken } = require("../middleware/authMiddleware");
 
 const upload = multer({ storage: multer.memoryStorage() });
 blogRouter.get("/", verifyToken, getPosts);
-blogRouter.post("/", createPost);
+blogRouter.post("/", verifyToken, createPost);
 blogRouter.get("/:id", getPost);
 blogRouter.put("/:id", updatePost);
 blogRouter.delete("/:id", deletePost);
