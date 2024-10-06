5682d502e67300d454a26968e2b1ef6f23d91682
Xyves <xodrixx@protonmail.com>
2024-10-06 19:40:26 +0200

    Change queries file from ts to js

---
+++ db/query.js
@@ -0,0 +1,111 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+// Posts CRUD
+function getDbPosts() {
+  return prisma.post.findMany({});
+}
+function getPost(id) {
+  return prisma.post.findFirst({ where: { id: id } });
+}
+function createPost(title, content, isPublished, userId) {
+  return prisma.post.create({
+    data: {
+      title,
+      content,
+      isPublished,
+      userId,
+    },
+  });
+}
+function updatePost(id, title, content, isPublished, userId) {
+  return prisma.post.update({
+    where: { id },
+    data: { title, content, isPublished },
+  });
+}
+function deletePost(id) {
+  return prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+}
+
+// COMMENTS CRUD
+function getCommentsByPostId(id) {
+  return prisma.comment.findUnique({ where: id });
+}
+
+function getUserComments(userId) {
+  // const authorId = "";
+  return prisma.comment.findMany({ where: { userId } });
+}
+function createComment(postId, email, nickname) {
+  return prisma.comment.create({
+    data: {
+      email,
+      nickname,
+    },
+    where: {},
+  });
+}
+function editComment(id, message) {
+  return prisma.comment.edit({ where: { id }, data: { message } });
+}
+function deleteComment(id) {
+  return prisma.comment.delete({ where: { id } });
+}
+async function createUser(nickname, password, email, role = "USER") {
+  return prisma.user.create({
+    data: {
+      nickname,
+      password,
+      email,
+      role,
+    },
+  });
+}
+// export function createUser({
+//   nickname:String,
+//   password,
+//   email,
+//   role = "User",
+// ){
+//   return prisma.user.create({
+//     data: {
+//       nickname,
+//       password,
+//       email,
+//       role,
+//     },
+//   });
+// }
+
+function deleteUser(id) {
+  return prisma.user.delete({ where: id });
+}
+async function getUserByName(nickname) {
+  console.log("Trying");
+  return await prisma.user.findUnique({ where: { nickname } });
+  // const match = await bcrypt.compare(password, user.password);
+  // if (match) {
+  //   return user;
+  // } else {
+  //   return null;
+  // }
+}
+module.exports = {
+  getDbPosts,
+  getPost,
+  getCommentsByPostId,
+  getUserComments,
+  getUserByName,
+  createPost,
+  createComment,
+  updatePost,
+  editComment,
+  createUser,
+  deletePost,
+  deleteComment,
+  deleteUser,
+};
