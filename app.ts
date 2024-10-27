260ae82fefeddd38174413be14d072713db6f5f0
Xyves <xodrixx@protonmail.com>
2024-10-27 07:25:06 +0100

    Update the Server to Support CORS

--- app.ts
+++ app.ts
@@ -8,9 +8,18 @@ const authRouter = require("./routes/authRouter");
 
 const commentsRouter = require("./routes/commentsRouter");
 
+const port = 3000;
+
 const cors = require("cors");
 app.use(cors());
-const port = 3000;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
