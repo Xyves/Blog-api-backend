7017b4d1d262a76083ef9bc74e50d55b179a5326
Xyves <xodrixx@protonmail.com>
2024-10-31 21:51:24 +0100

    Uncomment verifyToken

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -3,21 +3,21 @@ import { Request, Response, NextFunction } from "express";
 interface userAuthRequest extends Request {
   token: string; // or any other type
 }
-// export const verifyToken = (
-//   req: userAuthRequest,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const header = req.headers["authorization"];
-//   if (typeof header !== "undefined") {
-//     const bearer = header.split("");
-//     const token = bearer[1];
-//     req.token = token;
-//     next();
-//   } else {
-//     return res.status(303).json({ error: "Access denied!" });
-//   }
-// };
+export const verifyToken = (
+  req: userAuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const header = req.headers["authorization"];
+  if (typeof header !== "undefined") {
+    const bearer = header.split("");
+    const token = bearer[1];
+    req.token = token;
+    next();
+  } else {
+    return res.status(303).json({ error: "Access denied!" });
+  }
+};
 // const verifyToken = (
 //   req: userAuthRequest,
 //   res: Response,
--- routes/blogsRouter.js
+++ routes/blogsRouter.js
@@ -1,15 +1,12 @@
 const { Router } = require("express");
 const blogRouter = Router();
-
 const postController = require("../controllers/postController");
-const multer = require("multer");
 const { verifyToken } = require("../middleware/authMiddleware");
 
-const upload = multer({ storage: multer.memoryStorage() });
 blogRouter.get("/", postController.getPosts);
 blogRouter.post("/", verifyToken, postController.createPost);
 blogRouter.get("/:id", postController.getPost);
-blogRouter.put("/:id", postController.updatePost);
-blogRouter.delete("/:id", postController.deletePost);
+blogRouter.put("/:id", verifyToken, postController.updatePost);
+blogRouter.delete("/:id", verifyToken, postController.deletePost);
 
 module.exports = blogRouter;
