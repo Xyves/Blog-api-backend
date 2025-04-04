6fed9181c08d6c43b57268b89cc13055fbf02ff6
Xyves <xodrixx@protonmail.com>
2025-04-04 16:36:38 +0200

    Resolve CORS issue and add proper TypeScript typings

--- app.ts
+++ app.ts
@@ -6,15 +6,28 @@ const authRouter = require("./routes/authRouter");
 const commentsRouter = require("./routes/commentsRouter");
 const port = 3000;
 const cors = require("cors");
+import { Request, Response, NextFunction } from "express";
 app.use(
   cors({
     origin: "https://blog-api-xyves.netlify.app",
+    credentials: true,
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   })
 );
-app.options("*", cors());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://blog-api-xyves.netlify.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
